6a63ab3bf0b7e3d00626f96adbb3b808e7927188
Maheresio <zamalek.545r5@gmail.com>
2025-07-09 03:28:37 +0300

    refactor font resposive

--- lib/core/global/themes/app_theme_context.dart
+++ lib/core/global/themes/app_theme_context.dart
@@ -1,7 +1,7 @@
 // lib/core/theme/app_theme_context.dart
+import 'package:e_commerce/core/responsive/responsive_provider.dart';
 import 'package:flutter/material.dart';
 import '../../responsive/device_type.dart';
-import '../../responsive/device_type_holder.dart';
 
 class AppThemeContext {
   static late ThemeData theme;
@@ -20,7 +20,7 @@ class ThemeContextInitializer extends StatelessWidget {
     AppThemeContext.theme = Theme.of(context);
     AppThemeContext.color = Theme.of(context).colorScheme;
     AppThemeContext.text = Theme.of(context).textTheme;
-    AppThemeContext.deviceType = DeviceTypeHolder.value;
+    AppThemeContext.deviceType = ResponsiveProvider.of(context);
 
     return child;
   }
