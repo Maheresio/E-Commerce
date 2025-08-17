7dc4c1bb7ec2bef1f901c766157cb82b3e650bde
Maheresio <zamalek.545r5@gmail.com>
2025-08-17 09:34:01 +0300

    refactor: Clean up FirebaseAuthService by removing unused methods and reordering imports

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,7 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-import '../../features/auth/shared/domain/entity/user_entity.dart';
-
 abstract class FirebaseAuthService {
   Future<void> loginWithEmailAndPassword({
     required String email,
@@ -13,9 +9,4 @@ abstract class FirebaseAuthService {
     required String name,
     required String password,
   });
-  Stream<User?> authStateChanges();
-
-  Future<UserEntity?>  currentUser();
-
-  Future<void> logOut();
 }
--- lib/features/auth/shared/data/service/firebase_auth_service_impl.dart
+++ lib/features/auth/shared/data/service/firebase_auth_service_impl.dart
@@ -1,19 +1,16 @@
-import '../../../../../core/constants/firestore_constants.dart';
-import '../../../../../core/services/firestore_sevice.dart';
-import '../model/user_model.dart';
-import '../../domain/entity/user_entity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 import '../../../../../core/services/firebase_auth_service.dart';
+import '../../../../../core/services/firestore_sevice.dart';
+import '../../../../../core/constants/firestore_constants.dart';
+import '../model/user_model.dart';
 
 class FirebaseAuthServiceImpl implements FirebaseAuthService {
-  final FirebaseAuth firebaseAuth;
-  final FirestoreServices firestoreServices;
   const FirebaseAuthServiceImpl({
     required this.firebaseAuth,
     required this.firestoreServices,
   });
-
+  final FirebaseAuth firebaseAuth;
+  final FirestoreServices firestoreServices;
   @override
   Future<void> loginWithEmailAndPassword({
     required String email,
@@ -31,50 +28,19 @@ class FirebaseAuthServiceImpl implements FirebaseAuthService {
     required String name,
     required String password,
   }) async {
-    final userCredential = await firebaseAuth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
+    final UserCredential userCredential = await firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password);
     await userCredential.user?.updateDisplayName(name);
-
-    final uid = userCredential.user!.uid;
-
-    final user = UserModel(
+    final String uid = userCredential.user!.uid;
+    final UserModel user = UserModel(
       uid: uid,
       email: email,
       name: name,
       createdAt: DateTime.now(),
     );
-
     await firestoreServices.setData(
       path: FirestoreConstants.user(uid),
       data: user.toMap(),
     );
-  }
-
-  @override
-  Stream<User?> authStateChanges() => firebaseAuth.authStateChanges();
-
-  @override
-  Future<UserEntity?> currentUser() async {
-    final user = firebaseAuth.currentUser;
-    if (user == null) return null;
-
-    final docStream = firestoreServices.documentsStream(
-      path: FirestoreConstants.user(user.uid),
-      builder: (data, documentId) {
-        if (data == null) return null;
-        return UserModel.fromMap(data, documentId);
-      },
-    );
-
-    final doc = await docStream.first;
-
-    return doc;
-  }
-
-  @override
-  Future<void> logOut() async {
-    await firebaseAuth.signOut();
   }
 }
