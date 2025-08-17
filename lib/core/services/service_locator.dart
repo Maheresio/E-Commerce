2d8f4efce9ad81dfb358eb2137113567bdbbcfc7
Maheresio <zamalek.545r5@gmail.com>
2025-08-17 09:33:06 +0300

    feat: Enhance service locator with social authentication support and refactor dependencies

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,6 +1,7 @@
 import '../network/dio_client.dart';
 import 'firestore_sevice.dart';
 import 'supabase_storage_service.dart';
+import 'current_user_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 
@@ -14,8 +15,12 @@ import '../../features/auth/register/data/repositories/register_repository_impl.
 import '../../features/auth/register/domain/repositories/register_repository.dart';
 import '../../features/auth/register/domain/usecases/register_with_email_and_password_usecase.dart';
 import '../../features/auth/register/presentation/bloc/register_bloc.dart';
-import '../../features/auth/shared/data/service/firebase_auth_service_impl.dart';
-import 'firebase_auth_service.dart';
+import '../../features/auth/shared/data/data_source/auth_data_source.dart';
+import '../../features/auth/shared/data/repositories/auth_repository_impl.dart';
+import '../../features/auth/shared/domain/repositories/auth_repository.dart';
+import '../../features/auth/shared/domain/usecases/sign_in_with_google_usecase.dart';
+import '../../features/auth/shared/domain/usecases/sign_in_with_facebook_usecase.dart';
+import '../../features/auth/shared/presentation/bloc/auth_bloc.dart';
 
 final GetIt sl = GetIt.instance;
 
@@ -24,22 +29,23 @@ void serviceLocator() {
   sl.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
   sl.registerLazySingleton<FirestoreServices>(() => FirestoreServices.instance);
 
-  sl.registerLazySingleton<FirebaseAuthService>(
-    () => FirebaseAuthServiceImpl(
+  sl.registerLazySingleton<CurrentUserService>(
+    () => CurrentUserService(
       firebaseAuth: sl<FirebaseAuth>(),
       firestoreServices: sl<FirestoreServices>(),
     ),
   );
 
   sl.registerLazySingleton<DioClient>(DioClient.new);
-  sl.registerLazySingleton<SupabaseStorageService>(
-    SupabaseStorageService.new,
-  );
+  sl.registerLazySingleton<SupabaseStorageService>(SupabaseStorageService.new);
 
   // Auth Feature
   // -- Register
   sl.registerLazySingleton<RegisterDataSource>(
-    () => RegisterDataSourceImpl(sl<FirebaseAuthService>()),
+    () => RegisterDataSourceImpl(
+      firebaseAuth: sl<FirebaseAuth>(),
+      firestoreServices: sl<FirestoreServices>(),
+    ),
   );
   sl.registerLazySingleton<RegisterRepository>(
     () => RegisterRepositoryImpl(sl<RegisterDataSource>()),
@@ -53,7 +59,7 @@ void serviceLocator() {
 
   // -- Login
   sl.registerLazySingleton<LoginDataSource>(
-    () => LoginDataSourceImpl(sl<FirebaseAuthService>()),
+    () => LoginDataSourceImpl(firebaseAuth: sl<FirebaseAuth>()),
   );
   sl.registerLazySingleton<LoginRepository>(
     () => LoginRepositoryImpl(sl<LoginDataSource>()),
@@ -61,7 +67,33 @@ void serviceLocator() {
   sl.registerLazySingleton<LoginWithEmailAndPasswordUseCase>(
     () => LoginWithEmailAndPasswordUseCase(sl<LoginRepository>()),
   );
+
+  // -- Auth (Social Authentication)
+  sl.registerLazySingleton<AuthDataSource>(
+    () => AuthDataSourceImpl(
+      firebaseAuth: sl<FirebaseAuth>(),
+      firestoreServices: sl<FirestoreServices>(),
+    ),
+  );
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(sl<AuthDataSource>()),
+  );
+  sl.registerLazySingleton<SignInWithGoogleUseCase>(
+    () => SignInWithGoogleUseCase(sl<AuthRepository>()),
+  );
+  sl.registerLazySingleton<SignInWithFacebookUseCase>(
+    () => SignInWithFacebookUseCase(sl<AuthRepository>()),
+  );
+
+  // -- Blocs
   sl.registerFactory<LoginBloc>(
     () => LoginBloc(sl<LoginWithEmailAndPasswordUseCase>()),
+  );
+
+  sl.registerFactory<AuthBloc>(
+    () => AuthBloc(
+      signInWithGoogleUseCase: sl<SignInWithGoogleUseCase>(),
+      signInWithFacebookUseCase: sl<SignInWithFacebookUseCase>(),
+    ),
   );
 }
