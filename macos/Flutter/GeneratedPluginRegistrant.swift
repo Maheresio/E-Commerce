f020dcf2b4db3fea832098c75733c18349d0e4ed
Maheresio <zamalek.545r5@gmail.com>
2025-08-17 09:31:10 +0300

    feat: Add Flutter Secure Storage and Facebook Auth plugins for Linux, macOS, and Windows
    - Integrated flutter_secure_storage_linux and flutter_secure_storage_macos into the generated_plugin_registrant files for Linux and macOS respectively.
    - Updated generated_plugins.cmake to include flutter_secure_storage for both Linux and Windows.
    - Registered Facebook Auth plugin in macOS and added its dependencies in pubspec.lock.
    - Updated pubspec.yaml to include dependencies for bloc, flutter_facebook_auth, google_sign_in, and loading_animation_widget.
    - Updated pubspec.lock to reflect new versions and dependencies for various packages.
    - Ensured compatibility with Dart SDK version 3.8.0.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,12 @@ import Foundation
 import app_links
 import cloud_firestore
 import cloud_functions
+import facebook_auth_desktop
 import file_selector_macos
 import firebase_auth
 import firebase_core
+import flutter_secure_storage_macos
+import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
@@ -19,9 +22,12 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
+  FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
